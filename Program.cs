9b627ad1dd0d6421bedba932106733110816b146
Ambili das v s <ambili.kan@gmail.com>
2022-06-07 10:19:52 +0530

    Program is created for day of week

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WorkshopProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tWelcome to Day Of Week Program");
+            DayOfWeekClass.dayOfWeek();
+        }
+    }
+}
